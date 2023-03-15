988a7c1fdd4a4f6f27a03f56831c4462154a1fbc
Benson Arce <benson.arce@oracle.com>
2023-03-15 13:40:04 -0600

    initial commit

---
+++ src/main/java/com/selenium/uiautomation/UiAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.selenium.uiautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiAutomationApplication.class, args);
+	}
+
+}
